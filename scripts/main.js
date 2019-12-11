bf1d01cc9c3ac9d461a6a485b76fca2687e896e6
Katarzyna Maciejewska <katastic@o2.pl>
2019-12-11 11:46:07 +0100

    for

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,15 @@
-const image = 'kotek.png'
-console.log(image);
+const image1 = "https://picsum.photos/300/150";
+const image2 = "https://picsum.photos/300/150";
+const image3 = "https://picsum.photos/300/150";
+const images = [image1,image2,image3];
+images.push(image1);
+images.push(image2,image3);
+for (const image of images) {
+    console.log("To jest obrazek"+image)
+    console.log('KM')
+}
+for (let i=0;i<images.legth;i++){
+    const image = images [i];
+    console.log("To jest obrazek"+image)
+    console.log('KM')
+}
