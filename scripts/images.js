01d16a511d086a6ff170972fa4e8f6fea49a6cfb
Katarzyna Maciejewska <katastic@o2.pl>
2019-12-12 12:07:20 +0100

    a

--- scripts/images.js
+++ scripts/images.js
@@ -1,6 +1,19 @@
 const images = [
-    "https://picsum.photos/id/40/300/150",
-    "https://picsum.photos/id/50/300/150",
-    "https://picsum.photos/id/60/300/150",
+    {
+        url: "https://picsum.photos/id/40/300/150",
+        title: "SIERŚCIUCH",
+        author: "Piotr"
+    },
+    {
+        url: "https://picsum.photos/id/50/300/150",
+        title: "WODA",
+        author: "NONAME"
+    },
+    {
+        url: "https://picsum.photos/id/60/300/150",
+        title: "BIURKO",
+        author: "XYZ"
+    },
+    
 ];
 
--- scripts/main.js
+++ scripts/main.js
@@ -7,27 +7,45 @@
 //     console.log("To jest obrazek"+image)
 //     console.log('KM')
 // }
-const isEmpty = (images.length===0);
-const isEmpty2 = (images.length==0)
+const isEmpty = (images.length === 0);
+const isEmpty2 = (images.length == 0)
 function displayImages() {
-    console.log('Liczba obrazkow'+images.length);
-    console.log('Pierwszy element list'+images[0])
-    console.log('Drugi element list'+images[1])
+    console.log('Liczba obrazkow' + images.length);
+    console.log('Pierwszy element list' + images[0])
+    console.log('Drugi element list' + images[1])
 }
 function displayLastElement() {
-    console.log('Ost. element listy'+images[images.length-1])
-    console.log(876234/2378)
-    console.log(876234*2378)
+    console.log('Ost. element listy' + images[images.length - 1])
+    console.log(876234 / 2378)
+    console.log(876234 * 2378)
 }
 loader.show();
 
-function renderPhoto(url) {
-    const img = document.createElement('img');
-    img.src=url;
-    const main = document.querySelector('main')
-    main.append(img);
+function renderPhoto(photo) {
+    const $card = document.createElement('div');
+    $card.classList.add('card', 'col-3', 'mx-4');
+    
+    $card.classList.add('card');
+    $card.style.width = '30%'
+
+    const $img = document.createElement('img');
+    $img.src = photo.url;
+
+    const $body = document.createElement('div');
+    $body.classList.add('card-body');
+    
+    const $title = document.createElement('h5');
+    $title.classList.add('card-title');
+    $title.textContent = photo.title;
+
+    $card.append($img);
+    $card.append($body);
+    $body.append($title);
+
+    const $main = document.querySelector('main');
+    $main.append($card);
 }
-function displayPhotos (photos) {
+function displayPhotos(photos) {
     console.log(photos);
     for (const photo of photos) {
         renderPhoto(photo);
@@ -48,16 +66,16 @@ setTimeout(function () {
 function displayMessage(message) {
     const main = document.querySelector('main');
     // main.textContent=message;
-    main.innerHTML = '<p class="alert alert-info text-center">'+ message + '</p>' 
+    main.innerHTML = '<p class="alert alert-info text-center">' + message + '</p>'
 }
 function shouldDisplayPhotos() {
     const answer = prompt('Czy chcesz wyswietlic zdjecia?');
     return (answer.toLowerCase() === 'tak');
 }
-if (images.length>1 && images.length<10) {
+if (images.length > 1 && images.length < 10) {
     console.log('zdjecia istnieja');
     displayPhotos(images);
-}else{
+} else {
     console.log('zdjecia nie istnieja');
     displayMessage('Nie ma zdjec');
 }
